e7ac610aac7ed1189d7eb6808d06943a1ac5caae
vedianbnka <v.bunka238@gmail.com>
2025-09-22 15:53:30 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/com/vedianbunka/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/vedianbunka/lab_week_05/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.vedianbunka.lab_week_05.api.CatApiService
+import com.vedianbunka.lab_week_05.model.ImageData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     private val retrofit by lazy{
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
@@ -49,19 +51,22 @@ class MainActivity : AppCompatActivity() {
             1,
             "full"
         )
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        call.enqueue(object : Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(
                     MAIN_ACTIVITY,
                     "Failed to get response", t
                 )
             }
             override fun onResponse(
-                call: Call<String>, response:
-                Response<String>
+                call: Call<List<ImageData>>, response:
+                Response<List<ImageData>>
             ) {
                 if (response.isSuccessful) {
-                    apiResponseView.text = response.body()
+                    val image = response.body()
+                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    apiResponseView.text = getString(R.string.image_placeholder,
+                        firstImage)
                 } else {
                     Log.e(
                         MAIN_ACTIVITY,
---
+++ app/src/main/java/com/vedianbunka/lab_week_05/model/CatBreedData.kt
@@ -0,0 +1,6 @@
+package com.vedianbunka.lab_week_05.model
+
+data class CatBreedData(
+    val name: String,
+    val temperament: String
+)
